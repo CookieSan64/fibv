d0403fec0862157a64b544cfb4bbf123412caac6
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:21:56 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+author "ChouCookieSan"
+description "N.O.O.S.E"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map 'yes'
